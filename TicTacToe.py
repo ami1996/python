c16c3428feac3811a5b97e8ca350a829577c1857
Amit Kumar <singhamitkr1209@gmail.com>
2020-04-03 00:11:04 +0530

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -90,32 +90,32 @@ def CheckWinner():
 
     if winner == 1:
         messagebox.showinfo(title="Congratulations.", 
-            message="Player 1 is the winner")
+            message="Player 1 wins")
     elif winner == 2:
         messagebox.showinfo(title="Congratulations.", 
-            message="Player 2 is the winner")
+            message="Player 2 wins")
     elif mov == 9:
         messagebox.showinfo(title="Draw", 
-            message="It's a Draw!!")
+            message="Draw!!")
 
 def ButtonClick(id):
     global ActivePlayer
     global p1,p2
     global mov
 
-    if(ActivePlayer ==1):
+    if(ActivePlayer == 1):
         SetLayout(id,"X")
         p1.append(id)
-        mov +=1
-        root.title("Tic Tac Toe : Player 2")
-        ActivePlayer =2
+        mov += 1
+        root.title("Tic-Tac-Toe : Player 2")
+        ActivePlayer = 2
 
-    elif(ActivePlayer==2):
+    elif(ActivePlayer == 2):
         SetLayout(id,"O")
         p2.append(id)
-        mov +=1
-        root.title("Tic Tac Toe : Player 1")
-        ActivePlayer =1
+        mov += 1
+        root.title("Tic-Tac-Toe : Player 1")
+        ActivePlayer = 1
     CheckWinner()
 
 def AutoPlay():
