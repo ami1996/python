53768f8adaca7bdc582ff7dce368b06d46ff030c
Amit Kumar <singhamitkr1209@gmail.com>
2020-04-03 00:07:38 +0530

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -40,53 +40,61 @@ def CheckWinner():
     global mov 
     winner = -1
 
+    # row 1
     if(1 in p1) and (2 in p1) and (3 in p1):
         winner = 1
     if(1 in p2) and (2 in p2) and (3 in p2):
         winner = 2
 
+    # row 2
     if(4 in p1) and (5 in p1) and (6 in p1):
         winner = 1
     if(4 in p2) and (5 in p2) and (6 in p2):
         winner = 2
 
+    # row 3
     if(7 in p1) and (8 in p1) and (9 in p1):
         winner = 1
     if(7 in p2) and (8 in p2) and (9 in p2):
         winner = 2
 
+    #column 1
     if(1 in p1) and (4 in p1) and (7 in p1):
         winner = 1
     if(1 in p2) and (4 in p2) and (7 in p2):
         winner = 2
 
+    # column 2
     if(2 in p1) and (5 in p1) and (8 in p1):
         winner = 1
     if(2 in p2) and (5 in p2) and (8 in p2):
         winner = 2
 
+    # column 3
     if(3 in p1) and (6 in p1) and ( 9 in p1):
         winner = 1
     if(3 in p2) and (6 in p2) and (9 in p2):
         winner = 2
 
+    # diagonal 1
     if(1 in p1) and (5 in p1) and ( 9 in p1):
         winner = 1
     if(1 in p2) and (5 in p2) and (9 in p2):
         winner = 2
 
+    # diagonal 2
     if(3 in p1) and (5 in p1) and ( 7 in p1):
         winner = 1
     if(3 in p2) and (5 in p2) and (7 in p2):
         winner = 2
 
-    if winner ==1:
+    if winner == 1:
         messagebox.showinfo(title="Congratulations.", 
             message="Player 1 is the winner")
-    elif winner ==2:
+    elif winner == 2:
         messagebox.showinfo(title="Congratulations.", 
             message="Player 2 is the winner")
-    elif mov ==9:
+    elif mov == 9:
         messagebox.showinfo(title="Draw", 
             message="It's a Draw!!")
 
@@ -147,14 +155,12 @@ def Restart():
     global p1,p2,mov,ActivePlayer
     p1.clear(); p2.clear()
     mov,ActivePlayer = 0,1
-    root.title("Tic Tac Toe : Player 1")
+    root.title("Tic-Tac-Toe : Player 1")
     EnableAll()
-
-
-
+    
 
 root = Tk()
-root.title("Tic Tac toe : Player 1")
+root.title("Tic-Tac-toe : Player 1")
 st = ttk.Style()
 st.configure("my.TButton", font=('Chiller',24,'bold'))
 
